9e92aa652720a406d6fdf8564520b6fce6d1a22a
Ryan Brasseaux <rbrasseaux1@gmail.com>
2019-11-22 12:24:07 -0600

    Update TicTacToeAI.cpp

--- TicTacToeAI.cpp
+++ TicTacToeAI.cpp
@@ -1,7 +1,6 @@
 // TicTacToeAI.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
-#include "pch.h"
+#include "functions.h"
 #include <iostream>
 #include <ctime>
 #include <string>
